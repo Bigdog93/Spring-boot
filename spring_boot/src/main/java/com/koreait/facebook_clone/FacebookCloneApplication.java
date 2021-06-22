cb17c895c95d8f64bcc53fbb5a64d4d82bc85f18
BigDog93 <ljc9393@nate.com>
2021-06-22 10:16:18 +0900

    2021.06.22 mail sender 아이디 비밀번호 작성 전

--- spring_boot/src/main/java/com/koreait/facebook_clone/FacebookCloneApplication.java
+++ spring_boot/src/main/java/com/koreait/facebook_clone/FacebookCloneApplication.java
@@ -3,7 +3,7 @@ package com.koreait.facebook_clone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 스프링부트 부터는 톰캣 없이 얘가 서버를 실행시켜준다.
 public class FacebookCloneApplication {
 
     public static void main(String[] args) {
